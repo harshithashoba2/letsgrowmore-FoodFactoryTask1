2ca7206f7ea1c03cbf5516aa45eda87187fffbe7
Harshitha s <68117763+harshithashoba2@users.noreply.github.com>
2022-02-25 14:05:57 +0530

    Add files via upload

---
+++ 1st project/script.js
@@ -0,0 +1,31 @@
+
+function validate() 
+{
+   
+if( document.myForm.name.value == "" ) {
+alert( "Please provide your name!" );
+document.myForm.name.focus() ;
+return false;
+}
+if( document.myForm.email.value == "" ) {
+alert( "Please provide your Email!" );
+document.myForm.email.focus() ;
+return false;
+}
+if( document.myForm.phone.value == "" ) {
+alert( "Please provide your Contact Number!" );
+document.myForm.phone.focus() ;
+return false;
+}
+if( document.myForm.msg.value == "" ) {
+    alert( "Please provide your Message!" );
+    document.myForm.msg.focus() ;
+    return false;
+    }
+    
+alert("Details sent successfully!");
+
+
+
+}
+
